47c317e2b0838f34d23e473488e5635b6c2b1539
biaarruda126-collab <biaarruda126@gmail.com>
2026-05-04 09:27:33 -0300

    .

--- api-rest_node_pg/front_end/src/pages/Home/home.jsx
+++ api-rest_node_pg/front_end/src/pages/Home/home.jsx
@@ -1,55 +1,58 @@
-import styles from "./home.module.css";
-import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import styles from "./Home.module.css";
 
 function Home() {
   return (
     <div className={styles.app}>
-     
       <section className={styles.hero}>
-        <h1>
+        
+        <h1 className={styles.title}>
           wiss <span>mall</span>
         </h1>
 
         <div className={styles.divider}>
           <div></div>
-          <i className={styles.fa-solidfa-lock}></i>
+          <i className="fa-solid fa-lock"></i>
           <div></div>
         </div>
 
-        <p>
-          Seu shopping de presentes, novidades <br />e muito mais!
+        <p className={styles.subtitle}>
+          Seu shopping de presentes, novidades <br />
+          e muito mais!
         </p>
 
         <div className={styles.cards}>
+          
           <div className={styles.card}>
-            <i className={styles.fa-solidfa-phone}></i>
+            <i className="fa-solid fa-phone"></i>
             <h3>contato</h3>
             <span>Fale conosco</span>
           </div>
 
           <div className={styles.card}>
-            <i className={styles.fa-solidfa-user}></i>
+            <i className="fa-solid fa-user"></i>
             <h3>sobre nos</h3>
             <span>Conheça nossa loja</span>
           </div>
 
           <div className={styles.card}>
-            <i className={styles.fa-solidfa-motorcycle}></i>
+            <i className="fa-solid fa-motorcycle"></i>
             <h3>delivery</h3>
             <span>Receba em casa</span>
           </div>
 
           <div className={styles.card}>
-            <i className={styles.fa-solidfa-clipboard-list}></i>
+            <i className="fa-solid fa-clipboard-list"></i>
             <h3>cadastro</h3>
             <span>Crie sua conta</span>
           </div>
+
         </div>
 
         <div className={styles.scroll}>
-          <i className={styles.fa-solidfa-angle-down}></i>
+          <i className="fa-solid fa-angle-down"></i>
           <p>role para baixo</p>
         </div>
+
       </section>
     </div>
   );
